6b9fd25425a94c4cf48c83c375415077cc11ce38
Jennifer Carreno <jennifercarreno72@gmail.com>
2021-10-13 20:01:17 -0700

    added collapsable bios

---
+++ script.js
@@ -0,0 +1,15 @@
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    console.log(content);
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
